9e24839f0d57713a2fe5b32b79d1fce87861e5cf
Ariel1987 <arielpiresdealmeida@gmail.com>
2019-07-23 21:16:57 -0300

    current weather and place

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,37 @@ document.getElementById('time').innerHTML = today.toLocaleTimeString();
 document.getElementById('date').innerHTML = today.toLocaleDateString();
 
 
+
+navigator.geolocation.getCurrentPosition(function(position) {
+        
+    let latitude = position.coords.latitude;        
+    let longitude = position.coords.longitude;
+
+    function weatherBallon(position) {
+
+        fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + "66914e94781e7043c66bd385374a114d")
+        .then(function(resp) { 
+            return resp.json() 
+        }) // Convert data to json
+        .then(function(data) {
+            drawWeather(data);
+        })
+        .catch(function() {
+            // catch any errors
+        }); 
+    }
+    function drawWeather(data) {
+        let celcius = Math.round(parseFloat(data.main.temp)-273.15);
+        let fahrenheit = Math.round(((parseFloat(data.main.temp)-273.15)*1.8)+32);
+        let description = data.weather[0].description; 
+        document.getElementById('temp').innerHTML = celcius + '&deg;';
+        document.getElementById('location').innerHTML = data.name;
+    } 
+    
+    window.onload = function() {
+        weatherBallon(position);
+    }
+    
+});
+     
+    
