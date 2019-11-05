d672fb7443ebf6fa0fa97e5282d9712c016c56ee
Ariel1987 <arielpiresdealmeida@gmail.com>
2019-11-05 20:59:39 -0300

    Feature: WeatherApp

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,63 @@
+let days = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"];
+
+
+
 let today = new Date();
-document.getElementById('time').innerHTML = today.toLocaleTimeString();
-document.getElementById('date').innerHTML = today.toLocaleDateString();
+document.getElementById('time').innerHTML = today.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+document.getElementById('date').innerHTML = days[today.getDay()] + " " + today.toLocaleDateString();
 
 
 
-navigator.geolocation.getCurrentPosition(function(position) {
-        
-    let latitude = position.coords.latitude;        
-    let longitude = position.coords.longitude;
 
-    function weatherBallon(position) {
+document.getElementById('tomorrow').innerHTML = days[today.getDay() + 1]; 
+document.getElementById('day_after_tomorrow').innerHTML = days[today.getDay() + 2]; 
+document.getElementById('three_days_after_tomorrow').innerHTML = days[today.getDay() + 3]; 
+document.getElementById('four_days_after_tomorrow').innerHTML = days[today.getDay() + 4];
+document.getElementById('five_days_after_tomorrow').innerHTML = days[today.getDay() + 5];
+document.getElementById('six_days_after_tomorrow').innerHTML = days[today.getDay() +6];
 
-        fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + "66914e94781e7043c66bd385374a114d")
-        .then(function(resp) { 
-            return resp.json() 
-        }) // Convert data to json
-        .then(function(data) {
-            drawWeather(data);
-        })
-        .catch(function() {
-            // catch any errors
-        }); 
-    }
-    function drawWeather(data) {
-        let celcius = Math.round(parseFloat(data.main.temp)-273.15);
-        let fahrenheit = Math.round(((parseFloat(data.main.temp)-273.15)*1.8)+32);
-        let description = data.weather[0].description; 
-        document.getElementById('temp').innerHTML = celcius + '&deg;';
-        document.getElementById('location').innerHTML = data.name;
-    } 
-    
-    window.onload = function() {
-        weatherBallon(position);
-    }
-    
-});
-     
-    
+function getWeatherData() {
+	const options = {
+		enableHighAccuracy: true
+	};
+	navigator.geolocation.getCurrentPosition(
+		pos => {
+			fetch("http://api.openweathermap.org/data/2.5/weather?lat=" + pos.coords.latitude + "&lon=" + pos.coords.longitude + "&appid=" + "66914e94781e7043c66bd385374a114d")
+            .then(function(resp) { 
+                return resp.json() 
+            })
+            .then(function(data) {
+                drawWeather(data);
+            })
+            .catch(err => console.error(err));
+		},
+		error => console.error(error),
+		options
+	);
+}
+
+function drawWeather(data) {
+    let celcius = Math.round(parseFloat(data.main.temp)-273.15);
+    let fahrenheit = Math.round(((parseFloat(data.main.temp)-273.15)*1.8)+32);
+    let description = data.weather[0].description; 
+    let kmh = Math.round(parseFloat(data.wind.speed)*3.6);
+    document.getElementById('temp').innerHTML = celcius + '&deg;';
+    document.getElementById('location').innerHTML = data.name;
+    document.getElementById('wind_speed').innerHTML = kmh + ' km/h';
+    document.getElementById('rain').innerHTML = data.humidity + ' mm';
+} 
+
+window.onload = function() {
+    getWeatherData();
+}
+
+let searchBtn = document.getElementById('search-btn');
+let search = document.getElementById('search');
+let local = document.getElementById('location');
+searchBtn.addEventListener('click', () => {
+    search.style.display = 'inline-block';
+    search.focus();
+    local.style.display = 'none';
+    search.setAttribute('placeholder', 'Digite a cidade');
+}) 
+
